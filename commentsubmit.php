18032ac36ea2a24281eaea270109449f18e62675
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2012-10-28 17:03:43 +0100

    Move `mail` code to separate class
    All code involving headers, filtering email addresses and names, and all other activity
    is moved to a separate class.
    
    It makes the code clearer, easier to use, and easier to expand upon in the future.

--- commentsubmit.php
+++ commentsubmit.php
@@ -41,29 +41,11 @@ $HEADER_LINE_ENDING = "\r\n";
  * HERE BE CODE
  ****************************************************************************/
 
+require_once 'mail.php';
+
 function get_post_field($key, $defaultValue = "")
 {
 	return (isset($_POST[$key]) && !empty($_POST[$key])) ? $_POST[$key] : $defaultValue;
-}
-
-function filter_name($input)
-{
-	$rules = array( "\r" => '', "\n" => '', "\t" => '', '"'  => "'", '<'  => '[', '>'  => ']' );
-	return trim(strtr($input, $rules));
-}
-
-function filter_email($input)
-{
-	$rules = array( "\r" => '', "\n" => '', "\t" => '', '"'  => '', ','  => '', '<'  => '', '>'  => '' );
-	return strtr($input, $rules);
-}
-
-// Taken from http://php.net/manual/en/function.preg-replace.php#80412
-function filter_filename($filename, $replace = "")
-{
-	$reserved = preg_quote('\/:*?"<>|', '/'); //characters that are  illegal on any of the 3 major OS's
-	//replaces all characters up through space and all past ~ along with the above reserved characters
-	return preg_replace("/([\\x00-\\x20\\x7f-\\xff{$reserved}]+)/", $replace, $filename);
 }
 
 function get_post_data_as_yaml()
@@ -87,11 +69,8 @@ function get_post_data_as_yaml()
 	return $yaml_data;
 }
 
-
-$EMAIL_ADDRESS = filter_email($EMAIL_ADDRESS);
-
-$COMMENTER_NAME = filter_name(get_post_field('name', "Anonymous"));
-$COMMENTER_EMAIL_ADDRESS = filter_email(get_post_field('email', $EMAIL_ADDRESS));
+$COMMENTER_NAME = get_post_field('name', "Anonymous");
+$COMMENTER_EMAIL_ADDRESS = get_post_field('email', $EMAIL_ADDRESS);
 $COMMENTER_WEBSITE = get_post_field('link');
 $COMMENT_BODY = get_post_field('comment', "");
 $COMMENT_DATE = date($DATE_FORMAT);
@@ -101,58 +80,29 @@ $POST_ID = get_post_field('post_id', "");
 unset($_POST['post_id']);
 
 
-$yaml_data = "post_id: $POST_ID\n";
-$yaml_data .= "date: $COMMENT_DATE\n";
-$yaml_data .= get_post_data_as_yaml();
-
-$attachment_data = chunk_split(base64_encode($yaml_data));
-$attachment_date = date('Y-m-d-H-i-s');
-$attachment_name = filter_filename($POST_ID, '-') . "-comment-$attachment_date.yaml";
-
-
-$uid = md5(uniqid(time()));
-
 $subject = "Comment from $COMMENTER_NAME on '$POST_TITLE'";
-$subject = '=?UTF-8?B?'.base64_encode($subject).'?=';
 
 $message = "$COMMENT_BODY\n\n";
 $message .= "----------------------\n";
 $message .= "$COMMENTER_NAME\n";
 $message .= "$COMMENTER_WEBSITE\n";
 
-$header = "";
-function add_header($value = "")
-{
-	global $header, $HEADER_LINE_ENDING;
-	// Not passing any value will only add a line ending
-	$header .= $value . $HEADER_LINE_ENDING;
-}
+$mail = new Mail($subject, $message);
+$mail->set_from($EMAIL_ADDRESS, $COMMENTER_NAME);
+$mail->set_reply_to($COMMENTER_EMAIL_ADDRESS, $COMMENTER_NAME);
 
-add_header("From: $COMMENTER_NAME <$EMAIL_ADDRESS>");
-if (!empty($COMMENTER_EMAIL_ADDRESS)) add_header("Reply-To: $COMMENTER_NAME <$COMMENTER_EMAIL_ADDRESS>");
+$yaml_data = "post_id: $POST_ID\n";
+$yaml_data .= "date: $COMMENT_DATE\n";
+$yaml_data .= get_post_data_as_yaml();
 
-add_header("X-Mailer: PHP/" . phpversion());
-add_header("MIME-Version: 1.0");
-add_header("Content-Type: multipart/mixed; boundary=\"$uid\"");
-add_header();
-add_header("This is a multi-part message in MIME format.");
-add_header("--$uid"); // PLAIN-TEXT MESSAGE
-add_header("Content-Type: text/plain; charset=utf-8");
-add_header("Content-Transfer-Encoding: 8bit");
-add_header();
-add_header("$message");
-add_header();
-add_header("--$uid"); // COMMENT ATTACHMENT
-add_header("Content-Type: application/octet-stream; name=\"$attachment_name\"");
-add_header("Content-Transfer-Encoding: base64");
-add_header("Content-Disposition: attachment; filename=\"$attachment_name\"");
-add_header();
-add_header("$attachment_data");
-add_header();
-add_header("--$uid--");
+$attachment_date = date('Y-m-d-H-i-s');
+$attachment_name = Mail::filter_filename($POST_ID, '-') . "-comment-$attachment_date.yaml";
+
+$mail->header_line_ending = $HEADER_LINE_ENDING;
+$mail->set_attachment($yaml_data, $attachment_name);
 
 
-if (mail($EMAIL_ADDRESS, $subject, "", $header))
+if ($mail->send($EMAIL_ADDRESS))
 {
 	include $COMMENT_RECEIVED;
 }
