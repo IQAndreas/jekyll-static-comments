0f7514196c8cc33115121d76188bc501f3d125c9
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-09-28 09:17:51 +0200

    Allow comment to use YAML as frontmatter, fix Jekyll compatibility, et. al.
    I should really have separated these into separate commits...
    
    * Allow YAML to be contained in the frontmatter, and have the comment
    fill the "content" area of the page (this makes it much more readable!)
    * Fix compatibility issue for Jekyll >= 1.1.0 - see: https://github.com/mpalmer/jekyll-
    static-comments/issues/13
    * Allow both pages and posts to contain comments
    * Fix insignificant typo in the header comments

--- static_comments.rb
+++ static_comments.rb
@@ -1,6 +1,6 @@
 # Store and render comments as a static part of a Jekyll site
 #
-# See README.mdwn for detailed documentation on this plugin.
+# See README.md for detailed documentation on this plugin.
 #
 # Homepage: http://theshed.hezmatt.org/jekyll-static-comments
 #
@@ -21,19 +21,30 @@
 class Jekyll::Post
 	alias :to_liquid_without_comments :to_liquid
 	
-	def to_liquid
-		data = to_liquid_without_comments
-		data['comment_list'] = StaticComments::find_for_post(self)
+	def to_liquid(attrs = nil)
+		data = to_liquid_without_comments(attrs)
+		data['comment_list'] = StaticComments::find_for(self.site.source, data['id'])
+		data['comment_count'] = data['comment_list'].length
+		data
+	end
+end
+
+class Jekyll::Page
+	alias :to_liquid_without_comments :to_liquid
+	
+	def to_liquid(attrs = nil)
+		data = to_liquid_without_comments(attrs)
+		data['comment_list'] = StaticComments::find_for(self.site.source, data['id'])
 		data['comment_count'] = data['comment_list'].length
 		data
 	end
 end
 
 module StaticComments
-	# Find all the comments for a post
-	def self.find_for_post(post)
-		@comment_list ||= read_comments(post.site.source)
-		@comment_list[post.id]
+	# Find all the comments for a post or page with the specified id
+	def self.find_for(source, id)
+		@comment_list ||= read_comments(source)
+		@comment_list[id]
 	end
 	
 	# Read all the comments files in the site, and return them as a hash of
@@ -42,13 +53,34 @@ module StaticComments
 	def self.read_comments(source)
 		comment_list = Hash.new() { |h, k| h[k] = Array.new }
 		
-		Dir["#{source}/**/_comments/**/*"].sort.each do |comment|
-			next unless File.file?(comment) and File.readable?(comment)
-			yaml_data = YAML::load_file(comment)
+		Dir["#{source}/**/_comments/**/*"].sort.each do |comment_filename|
+			next unless File.file?(comment_filename) and File.readable?(comment_filename)
+			yaml_data = read_yaml(comment_filename)
 			post_id = yaml_data.delete('post_id')
 			comment_list[post_id] << yaml_data
 		end
 		
 		comment_list
+	end
+	
+	# Reads the specified file, parses the frontmatter and YAML, and returns the YAML data.
+	# Taken from Jekyll::Convertible, but with a few local modifications.
+	# Some code borrowed from http://stackoverflow.com/a/14232953/617937
+	def self.read_yaml(filename)
+		begin
+			file_contents = File.read(filename)
+			if (md = file_contents.match(/^(?<metadata>---\s*\n.*?\n?)^(---\s*$\n?)/m))
+				yaml_data = YAML.safe_load(md[:metadata])
+				yaml_data['comment'] = md.post_match
+			else # If there is no YAML header, it's all YAML. (reverse compatability with previous versions)
+				yaml_data = YAML.safe_load(file_contents)
+			end
+		rescue SyntaxError => e
+			puts "YAML Exception reading #{filename}: #{e.message}"
+		rescue Exception => e
+			puts "Error reading file #{filename}: #{e.message}"
+		end
+
+		yaml_data
 	end
 end
